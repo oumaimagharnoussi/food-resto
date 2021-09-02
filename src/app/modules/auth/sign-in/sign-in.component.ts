3a3270969818226cfda05f4d6b4a05fffb1ec1d1
alaa17 <alaa.assali@confledis.com>
2021-09-02 14:03:27 +0100

    commit

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -45,8 +45,8 @@ export class AuthSignInComponent implements OnInit
     {
         // Create the form
         this.signInForm = this._formBuilder.group({
-            username     : ['hughes.brian@company.com', Validators.required],
-            password  : ['admin', Validators.required],
+            username     : ['', Validators.required],
+            password  : ['', Validators.required],
             rememberMe: ['']
         });
     }
