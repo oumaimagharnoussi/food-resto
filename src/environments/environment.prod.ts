57cbef275d58314c13909719a66dba8379b493de
alaa17 <alaa.assali@confledis.com>
2021-10-29 14:48:00 +0100

    fix env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    BACKEND: 'https://food.dev.confledis.fr',
+    BACKEND: 'https://food.dev.confledis.fr', 
     API: 'https://food.dev.confledis.fr/api/',
     S3_url:'https://dev-rd-food.s3.eu-north-1.amazonaws.com/document/',
     SSE_url: 'https://food.dev.confledis.fr:3000'
