a0fc9b5752f68830022f7588aed18d78ab3a5f19
Madani120 <tchinda.abbas@gmail.com>
2022-06-23 17:38:06 +0200

    Added Manual address entry to bypass the map error #132

--- src/app/modules/auth/sign-up/sign-up.component.ts
+++ src/app/modules/auth/sign-up/sign-up.component.ts
@@ -10,6 +10,7 @@ import { DeviseService } from 'app/shared/services/devise.service';
 import { SpecialityService } from 'app/shared/services/speciality.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import Address from 'app/shared/Models/Address';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 
 import {Location, Appearance, GermanAddress} from '@angular-material-extensions/google-maps-autocomplete';
@@ -38,7 +39,8 @@ export class AuthSignUpComponent implements OnInit
     currencies: Object;
     devise:any=null
     speciality:any=null
-
+    town:any=null
+    commune:any=null
 
     public appearance = Appearance;
     public zoom: number;
@@ -47,8 +49,54 @@ export class AuthSignUpComponent implements OnInit
     public selectedAddress: PlaceResult;
     address: Address;
     selectedCountry="TN";
-
+    public checkMaps=true;
+    CongoTows = [
+      {value: 'Kinshasa', viewValue: 'Kinshasa'},
+      {value: 'Lubumbashi', viewValue: 'Lubumbashi'},
+      {value: 'Goma', viewValue: 'Goma'},
+      {value: 'Kananga', viewValue: 'Kananga'},
+      {value: 'Kisangani', viewValue: 'Kisangani'},
+    ];
+    TunisiaTows = [
+      {value: 'Tunis', viewValue: 'Tunis'},
+      {value: 'Sousse', viewValue: 'Sousse'},
+      {value: 'Sfax', viewValue: 'Sfax'},
+      {value: 'Nabeul', viewValue: 'Nabeul'},
+      {value: 'Djerba', viewValue: 'Djerba'},
+    ];
+    FrenchTows = [
+      {value: 'Paris', viewValue: 'Paris'},
+      {value: 'Lyon', viewValue: 'Lyon'},
+      {value: 'Toulouse', viewValue: 'Toulouse'},
+      {value: 'Grenoble', viewValue: 'Grenoble'},
+      {value: 'Lille', viewValue: 'Lille'},
+    ];
     
+    CongoCommunes = [
+      {value: 'Kinshasa', viewValue: 'Kinshasa'},
+      {value: 'Bandalungwa', viewValue: 'Bandalungwa'},
+      {value: 'Barumbu', viewValue: 'Barumbu'},
+      {value: 'Gombe', viewValue: 'Gombe'},
+      {value: 'Ville-province', viewValue: 'Ville-province'},
+    ];
+    TunisiaCommunes = [
+      {value: 'Tunis', viewValue: 'Tunis'},
+      {value: 'Sousse', viewValue: 'Sousse'},
+      {value: 'Sfax', viewValue: 'Sfax'},
+      {value: 'Nabeul', viewValue: 'Nabeul'},
+      {value: 'Médenine', viewValue: 'Médenine'},
+    ];
+    FrenchCommunes = [
+      {value: 'Paris', viewValue: 'Paris'},
+      {value: 'Lyon', viewValue: 'Lyon'},
+      {value: 'Bobigny', viewValue: 'Bobigny'},
+      {value: 'Saint-Denis', viewValue: 'Saint-Denis'},
+      {value: 'Nanterre', viewValue: 'Nanterre'},
+      {value: 'Nanterre', viewValue: 'Nanterre'},
+    ];
+
+    towns;
+    communes;
 
     /**
      * Constructor
@@ -83,7 +131,8 @@ export class AuthSignUpComponent implements OnInit
      */
     ngOnInit(): void
     {
-        
+        this.towns=this.TunisiaTows;
+        this.communes=this.TunisiaCommunes;
         this.zoom = 10;
         this.latitude = 52.520008;
         this.longitude = 13.404954;
@@ -118,7 +167,12 @@ export class AuthSignUpComponent implements OnInit
                 description  :  ['', Validators.required],
            
             
-                restaurantTel:  ['', Validators.required]
+                restaurantTel:  ['', Validators.required],
+
+                street : ['', Validators.required],
+                streetNb  : ['', Validators.required],
+                zipcode : ['', Validators.required],
+                
             }
         );
     }
@@ -147,19 +201,34 @@ export class AuthSignUpComponent implements OnInit
 
         const restaurantInfo= new Restaurant();
         restaurantInfo.businessAddress=new Address();
+        restaurantInfo.description=this.signUpForm.value.description;
+        restaurantInfo.name=this.signUpForm.value.restauName;
+       
+        if(this.checkMaps){
+        restaurantInfo.businessAddress.locality=this.address.locality;
         restaurantInfo.businessAddress.administrativeAreaLevel1=this.address.administrativeAreaLevel1
         restaurantInfo.businessAddress.country=this.address.country;
-        restaurantInfo.description=this.signUpForm.value.description;
-        restaurantInfo.businessAddress.locality=this.address.locality;
-        restaurantInfo.name=this.signUpForm.value.restauName;
         restaurantInfo.businessAddress.postalCode=this.address.postalCode;
         restaurantInfo.businessAddress.route=this.address.route;
         restaurantInfo.businessAddress.latitude=this.address.latitude;
         restaurantInfo.businessAddress.longitude=this.address.longitude;
+        restaurantInfo.businessAddress.streetNumber=this.address.streetNumber;
+        }else{
+              
+        restaurantInfo.businessAddress.locality=this.town;
+        restaurantInfo.businessAddress.administrativeAreaLevel1=this.commune
+        restaurantInfo.businessAddress.country=this.selectedCountry;
+        restaurantInfo.businessAddress.postalCode=this.signUpForm.value.zipcode;
+        restaurantInfo.businessAddress.route=this.signUpForm.value.street;
+        restaurantInfo.businessAddress.latitude=this.latitude;
+        restaurantInfo.businessAddress.longitude=this.longitude;
+        restaurantInfo.businessAddress.streetNumber=this.signUpForm.value.streetNb;
+        }
+       
         restaurantInfo.currency='api/currencies/'+this.devise
         restaurantInfo.speciality='api/specialities/'+this.speciality
        // restaurantInfo.logo=this.restaurant.logo;
-        restaurantInfo.businessAddress.streetNumber=this.address.streetNumber;
+       
         restaurantInfo.restaurantTel=this.signUpForm.value.restaurantTel;
 
         // Sign up
@@ -209,11 +278,36 @@ export class AuthSignUpComponent implements OnInit
         this.address.locality=$event.locality.long
      //   this.address.postalCode=$event.postalCode.toString()
         this.address.route=$event.streetName
+        this.checkMaps = true;
      
       }
-
+      updateCheckMaps(ck: MatCheckboxChange){
+        this.checkMaps = !ck.checked;
+      }
       show(){
           console.log(this.selectedCountry)
+          switch(this.selectedCountry){
+            case 'TN': { 
+              this.towns=this.FrenchTows;
+              this.communes=this.FrenchCommunes;
+              break; 
+           } 
+           case "RCD": { 
+              this.towns=this.CongoTows;
+              this.communes=this.CongoCommunes;
+              break; 
+           } 
+           case 'FR': { 
+              this.towns=this.FrenchTows;
+              this.communes=this.FrenchCommunes;
+            break; 
+         } 
+           default: { 
+            this.towns=this.FrenchTows;
+            this.communes=this.FrenchCommunes;
+              break; 
+           } 
+          }
           
        }
 }
